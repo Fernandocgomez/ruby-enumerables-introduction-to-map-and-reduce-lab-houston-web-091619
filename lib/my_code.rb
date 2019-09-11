b448faba890e5edd89c7d696df6d0bce3099f7a7
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-09-11 20:49:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -86,9 +86,10 @@ end
 def reduce_to_any_true(source_array) 
   i = 0
   while i < source_array.length do 
-    return true if source_array[i] == true else return false
+    return true if source_array[i] == true
     i += 1
   end
+  return false
 end
 
 
