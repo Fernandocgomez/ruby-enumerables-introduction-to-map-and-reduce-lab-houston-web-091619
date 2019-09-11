a3098c2f0a22b968e062e2bc5bb39f9b2ab5ab3c
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-09-11 20:23:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,93 @@
 # My Code here....
+
+def map_to_negativize(source_array)
+  
+  i = 0
+  new_array = []
+  
+  while i < source_array.length do
+    
+    new_array << source_array[i] * -1
+    i += 1
+    
+  end
+  
+  new_array
+  
+end
+
+
+def map_to_no_change(source_array) 
+  
+  source_array
+  
+end
+
+def map_to_double(source_array) 
+  
+  i = 0
+  new_array = []
+  
+  while i < source_array.length do
+    
+    new_array << source_array[i] * 2
+    
+    i += 1
+    
+  end
+  
+  new_array
+  
+end
+
+
+def map_to_square(source_array) 
+  
+  i = 0
+  new_array = []
+  
+  while i < source_array.length do
+    
+    new_array << source_array[i] * source_array[i]
+    
+    i += 1
+    
+  end
+  
+  new_array
+  
+end
+
+def reduce_to_total(source_array, starting_point=0)
+  
+  new_array = starting_point
+  i = 0
+  
+  while i < source_array.length do 
+    new_array += source_array[i]
+    i += 1
+    
+  end
+  
+  new_array
+  
+end 
+
+
+def reduce_to_true(array)
+  i = 0
+  while i < array.length do 
+    return false if array[i] == false
+  end
+  return true
+end
+
+
+
+
+
+
+
+
+
+
